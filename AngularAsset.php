178262869bcb2e37b38797329964f14b1eca465e
B@RT <bartlab@gmail.com>
2016-03-02 15:59:29 +0200

    Update AngularAsset.php

--- AngularAsset.php
+++ AngularAsset.php
@@ -7,7 +7,8 @@ use yii\web\AssetBundle;
 class AngularAsset extends AssetBundle
 {
     public $depends = [
-        'maybeworks\angular\AngularLocaleAsset',
+        'maybeworks\angular\AngularMainAsset',
+        'maybeworks\angular\AngularI18nAsset',
         'maybeworks\angular\AngularRouteAsset',
         'maybeworks\angular\AngularSanitizeAsset',
         'maybeworks\angular\AngularAnimateAsset',
